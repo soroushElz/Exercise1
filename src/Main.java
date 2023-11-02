2ae792aeed847affade0b09c6a8a86d06b466602
Soroush <Soroush.yz.97@Gmail.com>
2023-11-02 18:44:24 +0330

    feature 1st commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
  public static void main(String args[])	{
 	 
-	 System.out.printf("message:+ %s%n"," third Commit");
+	 System.out.printf("message:+ %s%n"," Changed in feaature branch 1s commmit");
  }
 	
 }
