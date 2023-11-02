6b640953ce96ae3c0b90e3f334e4a45ed50d6573
Soroush <149657683+soroushElz@users.noreply.github.com>
2023-11-02 18:37:17 +0330

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
  public static void main(String args[])	{
 	 
-	 System.out.printf("message:+ %s%n"," second Commit");
+	 System.out.printf("message:+ %s%n"," third Commit");
  }
 	
 }
