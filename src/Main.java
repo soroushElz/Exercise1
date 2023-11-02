55d3e5f436906148cdc8a4c938df9690b0caaf89
Soroush <Soroush.yz.97@Gmail.com>
2023-11-02 19:02:02 +0330

    feature fixed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
  public static void main(String args[])	{
 	 
-	 System.out.printf("message:+ %s%n"," Changed in feaature branch Second commmit");
+	 System.out.printf("message:+ %s%n"," Changed in feature branch commmit");
  }
 	
 }
