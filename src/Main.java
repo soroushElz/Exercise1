30abebf5edea97ebb79792ea0b42cf9f553db26c
Soroush <Soroush.yz.97@Gmail.com>
2023-11-03 21:38:15 +0330

    second change in feature

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
  public static void main(String args[])	{
 	 
-	 System.out.printf("message:+ %s%n","Saved in feature branch");
+	 System.out.printf("message:+ %s%n","second Change in feature branch");
 
  }
 	
